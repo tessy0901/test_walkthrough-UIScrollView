baa72157d143a72471946c9df81479931076beaf
tessy0901 <tessy0901@gmail.com>
2019-12-18 13:32:32 +0900

    Initial Commit

---
+++ test_walkthrough_UIScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_walkthrough_UIScroll
+//
+//  Created by 手嶋慧太 on 2019/12/18.
+//  Copyright © 2019 手嶋慧太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test_walkthrough_UIScrollTests/test_walkthrough_UIScrollTests.swift
@@ -0,0 +1,34 @@
+//
+//  test_walkthrough_UIScrollTests.swift
+//  test_walkthrough_UIScrollTests
+//
+//  Created by 手嶋慧太 on 2019/12/18.
+//  Copyright © 2019 手嶋慧太. All rights reserved.
+//
+
+import XCTest
+@testable import test_walkthrough_UIScroll
+
+class test_walkthrough_UIScrollTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
